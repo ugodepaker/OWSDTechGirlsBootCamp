ff2f96d02d71935f35ad9bd7b45d4acd82d1024f
ugodepaker <64331178+ugodepaker@users.noreply.github.com>
2024-02-26 17:11:45 +0100

    Add files via upload

---
+++ sales_app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+# Load the pre-trained model
+@st.cache_data
+def load_model():
+    return joblib.load('rf_regressor.joblib')
+
+model = load_model()
+
+# Set background color
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #FFB6C1; 
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Welcome message
+st.title('Welcome to the Sales Prediction Platform!')
+st.write("This platform is designed to help you predict sales based on your advertising expenditure.")
+
+# Introduction to advertising channels and costs
+st.header('Advertising Channels and Costs')
+st.write("Before we make predictions, let's introduce you to the advertising channels ")
+st.write("- TV Advertisment")
+st.write("- Radio Advertisment")
+st.write("- Newspaper Advertisment")
+st.write("Please input the values for advertising expenditure.")
+
+# Input fields for user input
+st.sidebar.header('Advertising Channels Parameters')
+tv = st.sidebar.text_input('TV Advertising', '150.0')
+radio = st.sidebar.text_input('Radio Advertising', '25.0')
+newspaper = st.sidebar.text_input('Newspaper Advertising', '50.0')
+
+# Submit button
+if st.sidebar.button('Submit'):
+    # Convert input values to float
+    tv = float(tv)
+    radio = float(radio)
+    newspaper = float(newspaper)
+    
+    # Make predictions
+    input_data = pd.DataFrame({'TV': [tv], 'Radio': [radio], 'Newspaper': [newspaper]})
+    prediction = model.predict(input_data)
+
+    # Display prediction
+    st.header('Sales Prediction')
+    st.success(f"Predicted Sales: {prediction[0]}")
